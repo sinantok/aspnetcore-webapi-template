f043be4110a568f7a828012ff88da7e09c803fa3
Sinan Tok <sinantok@outlook.com>
2020-10-01 00:01:48 +0300

    identity table seeds

--- src/Presentations/WebApi/Program.cs
+++ src/Presentations/WebApi/Program.cs
@@ -2,18 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Identity.Seeds;
 namespace WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await DefaultRoles.SeedAsync(userManager, roleManager);
+                    await DefaultSuperAdmin.SeedAsync(userManager, roleManager);
+                }
+                catch (Exception ex) { }
+                finally { }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
