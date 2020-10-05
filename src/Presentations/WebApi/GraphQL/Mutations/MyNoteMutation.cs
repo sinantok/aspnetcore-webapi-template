2a5774a505607f8c6832f9ff18c5811c90e6a7d0
Sinan Tok <sinantok@outlook.com>
2020-10-06 00:20:14 +0300

    graphql structure has been created

---
+++ src/Presentations/WebApi/GraphQL/Mutations/MyNoteMutation.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.GraphQL.Mutations
+{
+    public class MyNoteMutation : ObjectGraphType
+    {
+        public MyNoteMutation()
+        {
+        }
+    }
+}
---
+++ src/Presentations/WebApi/GraphQL/Queries/MyNoteQuery.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.GraphQL.Queries
+{
+    public class MyNoteQuery : ObjectGraphType
+    {
+        public MyNoteQuery()
+        {
+        }
+    }
+}
