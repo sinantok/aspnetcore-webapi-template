b47a264aedc53a856dcf096e2e31d70f761c5f9c
Sinan Tok <sinantok@outlook.com>
2020-10-08 00:20:42 +0300

    generic repo has been created

---
+++ src/Libraries/Data/Repos/GenericRepository.cs
@@ -0,0 +1,78 @@
+﻿using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Models.DbEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Data.Repos
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+        public GenericRepository(ApplicationDbContext context = null)
+        {
+            _context = context;
+        }
+
+        public bool BulkInsert(List<T> entities)
+        {
+            _context.ChangeTracker.AutoDetectChangesEnabled = false;
+            try
+            {
+                _context.Set<T>().AddRange(entities);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public T Find(Expression<Func<T, bool>> match)
+        {
+            return _context.Set<T>().FirstOrDefault(match);
+        }
+
+        public List<T> FindAll(Expression<Func<T, bool>> match)
+        {
+            return _context.Set<T>().Where(match).ToList();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public T Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            if (entity == null)
+                return null;
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+            return entity;
+        }
+    }
+}
---
+++ src/Libraries/Data/Repos/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using Models.DbEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Data.Repos
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        T Find(Expression<Func<T, bool>> match);
+        List<T> FindAll(Expression<Func<T, bool>> match);
+        T Insert(T entity);
+        bool BulkInsert(List<T> entities);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
