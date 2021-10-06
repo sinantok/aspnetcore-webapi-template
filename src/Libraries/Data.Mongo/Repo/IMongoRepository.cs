16bbb5262f456e2512b7bcc2fe1165edd450c7de
Sinan Tok <sinan.tok@demirorenteknoloji.com>
2021-10-06 23:08:27 +0300

    mongo repository added

---
+++ src/Libraries/Data.Mongo/Repo/IMongoRepository.cs
@@ -0,0 +1,75 @@
+﻿using Data.Mongo.Collections;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Data.Mongo.Repo
+{
+    public interface IMongoRepository<T> where T : MongoBaseDocument
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> GetAsync(string id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task AddAsync(T entity);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task UpdateAsync(T entity);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteAsync(string id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="field"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task<ICollection<T>> FindDistinctAsync(string field, FilterDefinition<T> filter);
+    }
+}
---
+++ src/Libraries/Data.Mongo/Repo/MongoRepository.cs
@@ -0,0 +1,81 @@
+﻿using Data.Mongo.Attributes;
+using Data.Mongo.Collections;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Mongo.Repo
+{
+    public class MongoRepository<T> : IMongoRepository<T> where T : MongoBaseDocument
+    {
+
+        protected IMongoCollection<T> Collection { get; }
+
+        public MongoRepository(IMongoDatabase database)
+        {
+            string collectionName;
+
+            var att = Attribute.GetCustomAttribute(typeof(T), typeof(BsonCollectionAttribute));
+
+            if (att != null)
+            {
+                collectionName = ((BsonCollectionAttribute)att).CollectionName;
+            }
+            else
+            {
+                collectionName = typeof(T).Name;
+            }
+
+            //var collectionName = typeof(TEntity).GetCustomAttribute<BsonCollectionAttribute>(false).CollectionName;
+            Collection = database.GetCollection<T>(collectionName);
+
+        }
+
+        public async Task AddAsync(T entity)
+            => await Collection.InsertOneAsync(entity);
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<T>> FindDistinctAsync(string field, FilterDefinition<T> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
