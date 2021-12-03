6d0eb5b1c99be53182eadf86e018150899925ade
sinantok <sinantok@outlook.com>
2021-12-04 00:03:34 +0300

    LogController created

---
+++ src/Presentations/WebApi/Controllers/LogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.DTOs.Log;
+using Models.ResponseModels;
+using Services.Interfaces;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly ILoginLogService _loginLogService;
+        public LogController( ILoginLogService loginLogService)
+        {
+            _loginLogService = loginLogService;
+        }
+        [HttpGet("get")]
+        public IActionResult GetUserAuthLogs(string email)
+        {
+            return Ok(new BaseResponse<IReadOnlyList<LogDto>>(null, $"User Log List"));
+        }
+        public IActionResult Index()
+        {
+            return Content("");
+        }
+    }
+}
